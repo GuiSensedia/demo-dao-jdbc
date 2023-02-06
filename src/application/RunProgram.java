0fe7a1ee2cbfb9766874bed79bcf9af6b5fd1adc
GuiSensedia <guilherme.santos@sensedia.com>
2023-02-06 09:29:53 -0300

    Implementando e instanciando FindByDepartment - TEST2

--- src/application/RunProgram.java
+++ src/application/RunProgram.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,10 +14,17 @@ public class RunProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("=== TEST 1: seller - findById ===");
+		System.out.println("\n=== TEST 1: seller - findById ===");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		System.out.println();
+		System.out.println("=== TEST 2: seller - findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller obj : list) {
+			System.out.println(seller);
+		}
 		
 	}
 
